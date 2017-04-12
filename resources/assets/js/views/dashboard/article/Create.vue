3e32da3222a24a1965b209da6d6aa657d1829d08
d4smart <d4smart@foxmail.com>
2017-04-12 01:38:33 +0000

    initial commit

---
+++ resources/assets/js/views/dashboard/article/Create.vue
@@ -0,0 +1,28 @@
+<template>
+    <vue-form :title="$t('form.create_article')">
+        <div slot="buttons">
+            <router-link to="/dashboard/articles" class="btn btn-default" exact>{{ $t('form.back') }}</router-link>
+        </div>
+        <div slot="content">
+            <article-form></article-form>
+        </div>
+    </vue-form>
+</template>
+
+<script>
+import ArticleForm from './Form'
+
+export default {
+    components: { ArticleForm },
+}
+</script>
+
+<style lang="scss">
+    .editor-toolbar.fullscreen {
+        z-index: 1000 !important;
+    }
+
+    .CodeMirror-fullscreen {
+        z-index: 1000 !important;
+    }
+</style>
